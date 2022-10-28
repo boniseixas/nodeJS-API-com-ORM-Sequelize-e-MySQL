bb1b72e9b28874b57ae048a86ddcde6fccd84c48
boniseixas <bonisousa73@gmail.com>
2022-10-28 15:13:15 -0300

    CRUD com Sequelize

--- api/controllers/PessoaController.js
+++ api/controllers/PessoaController.js
@@ -1,11 +1,59 @@
 const database = require('../models')
 
 class PessoaController {
-   static async pegaTodasAsPessoas(req, res) {
+   static async buscaTodasAsPessoas(req, res) {
       try {
          const todasAsPessoas = await database.Pessoas.findAll()
          return res.status(200).json(todasAsPessoas)
       } catch(error) {
+         return res.status(500).json(error.message)
+      }
+   }
+
+   static async buscaPessoaPorId(req, res) {
+      const { id } = req.params
+      try {
+         const umaPessoa = await database.Pessoas.findOne( {
+            where: {
+               id: Number(id)
+            }
+         } )
+         return res.status(200).json(umaPessoa)
+      } catch(error) {
+         return res.status(500).json(error.message)
+      }
+   }
+
+   static async criaPessoa(req, res) {
+      const novaPessoa = req.body
+      try {
+         const criaNovaPessoa = await database.Pessoas.create(novaPessoa)
+         return res.status(200).json(criaNovaPessoa)
+      } catch (error) {
+         return res.status(500).json(error.message)
+      }
+   }
+
+   static async atualizaPessoa(req, res) {
+      const { id } = req.params
+      const atualizaRegistro = req.body
+      try {
+         await database.Pessoas.update(atualizaRegistro, { where: { id: Number(id) }}
+         )
+         const pessoaAtualizada = await database.Pessoas.findOne( { where: { id: Number(id) }}
+         )
+         return res.status(200).json(pessoaAtualizada)
+      } catch (error) {
+         return res.status(500).json(error.message)
+      }
+   }
+
+   static async excluiPessoa(req, res) {
+      const { id } = req.params
+      try {
+         await database.Pessoas.destroy({ where: { id: Number(id) }})
+         return res.status(200).json({ mensagem: `id ${id} deletado!`})
+      } catch (error) {
          return res.status(500).json(error.message)
       }
    }
--- api/routes/pessoasRoute.js
+++ api/routes/pessoasRoute.js
@@ -5,6 +5,10 @@ const router = Router()
 
 // const pController = new PessoaController // não precisa criar um nova instancia de PessoaController, porque esse método foi declarado com estático na classe PessoaController
 
-router.get('/pessoas', PessoaController.pegaTodasAsPessoas)
+router.get('/pessoas', PessoaController.buscaTodasAsPessoas)
+router.get('/pessoas/:id', PessoaController.buscaPessoaPorId)
+router.post('/pessoas', PessoaController.criaPessoa)
+router.put('/pessoas/:id', PessoaController.atualizaPessoa)
+router.delete('/pessoas/:id', PessoaController.excluiPessoa)
 
 module.exports = router
